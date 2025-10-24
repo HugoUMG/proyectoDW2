e5b49abb9b8ccccceebd6d4feed2bb82ae8bcd37
HugoUMG <149248893+HugoUMG@users.noreply.github.com>
2025-10-23 23:25:33 -0600

    Remove presentation component from root layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BalanceGeneralComponent } from './components/balance-general/balance-general.component';
 import { TransaccionesComponent } from './components/transacciones/transacciones.component';
-import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PresentacionComponent, BalanceGeneralComponent, TransaccionesComponent],
+  imports: [CommonModule, BalanceGeneralComponent, TransaccionesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
